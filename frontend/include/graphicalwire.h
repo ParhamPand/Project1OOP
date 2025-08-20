258a2def6fdd6daa959139861b389555b41d4295
Parham <parhampand1385@gmail.com>
2025-08-20 20:04:38 +0330

    Transient analysis finished

--- frontend/include/graphicalwire.h
+++ frontend/include/graphicalwire.h
@@ -1,26 +1,35 @@
 #ifndef GRAPHICALWIRE_H
 #define GRAPHICALWIRE_H
 
-#include <QGraphicsPathItem>
+// به جای QGraphicsPathItem از QGraphicsObject استفاده کنید
+#include <QGraphicsObject>
+#include <QPainterPath>
+#include <QPen>
 
 class GraphicalComponent; // Forward declaration
 
-class GraphicalWire : public QGraphicsPathItem
+// ارث‌بری را از QGraphicsPathItem به QGraphicsObject تغییر دهید
+class GraphicalWire : public QGraphicsObject
 {
+Q_OBJECT // <-- ماکروی Q_OBJECT را اضافه کنید
+
 public:
     GraphicalWire(GraphicalComponent *startComp, int startTerminal, GraphicalComponent *endComp, int endTerminal, QGraphicsItem *parent = nullptr);
 
-    // Add any other necessary functions here
+    QRectF boundingRect() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
 public slots:
     void updatePosition();
-
 
 private:
     GraphicalComponent *m_startComponent;
     GraphicalComponent *m_endComponent;
     int m_startTerminalIndex;
     int m_endTerminalIndex;
+
+    QPainterPath m_path;
+    QPen m_pen;
 };
 
-#endif // GRAPHICALWIRE_H
+#endif
